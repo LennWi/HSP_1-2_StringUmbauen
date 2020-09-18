7e25a4dcbbc9ea7c1bdfb84a489f403c3193b8e0
LennWi <70192362+LennWi@users.noreply.github.com>
2020-09-18 12:56:35 +0200

    Projektdateien hinzufügen.

---
+++ Uebung2_HSP_2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung2_HSP_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            string hw = " HelloWorld Welt";
+            double zahl = 2.0;
+
+            //char[] spaces = { ' ' };
+
+            string[] hwSplitUp = hw.Split(' ');     //geht auch mit 'spaces' wenn char array genutzt wird
+
+            string hwNew = hw.Replace(" HelloWorld Welt", String.Format("{0} {1:0.000}", hwSplitUp[1], zahl));
+
+            Console.WriteLine(hwNew);
+
+            Console.ReadKey();
+        }
+    }
+}
